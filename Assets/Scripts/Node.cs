69e999959ac08836ae32c8668057addaac155f1d
Shanzong <songfq24@gmail.com>
2020-05-10 19:50:42 -0700

    Node show up

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject geometry;
+
+    public float scaleTime = 0.3f;
+
+    public iTween.EaseType easeType = iTween.EaseType.easeInExpo;
+
+    public bool autoRun;
+
+    public float delay = 1f;
+
+    void Start()
+    {
+        if (geometry != null)
+        {
+            geometry.transform.localScale = Vector3.zero;
+        }
+        if (autoRun)
+        {
+            ShowGeometry();
+        }
+    }
+
+    private void ShowGeometry()
+    {
+        if (geometry != null)
+        {
+            iTween.ScaleTo(geometry, iTween.Hash("time", scaleTime, "scale", Vector3.one, "easetype", easeType, "delay", delay));
+        }
+    }
+}
